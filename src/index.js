494bca4efab798b852ac0de7b14f8c7ff6301f70
polyakov-artem <sunabozu13@gmail.com>
2021-03-11 20:00:03 +0400

    feat: add max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) return 0;
+  return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) return 0;
+  return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) return 0;
+  return array.reduce((a,b)=>a+b,0)/array.length;
 }
